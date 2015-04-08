5062634288ab7a31671868be16d62d2bd88a1744
Henrique Jung <henriquenj@gmail.com>
2015-04-07 22:38:27 -0300

    Added metadata e register functions

--- BlenderPlugin/RenderGirlBlender.py
+++ BlenderPlugin/RenderGirlBlender.py
@@ -1,10 +1,23 @@
 import bpy
 
-# TODO (Henrique Jung): added tags for proper identification inside blender
+# Metadata for blender internal plugin system
+bl_info = {
+    "name": "RenderGirl",
+    "author": "Henrique Jung",
+    "version": (0, 0, 1),
+    "blender": (2, 70, 0), # TODO(Henrique Jung): test with other versions of blender
+    "location": "Info header, render engine menu",
+    "description": "RenderGirl raytracer for Blender",
+    "warning": "Still on very early stage of development!",
+    "wiki_url": "https://github.com/henriquenj/rendergirl",
+    "tracker_url": "",
+    "support": 'TESTING',
+    "category": "Render"}
+
 
 class RenderGirl(bpy.types.RenderEngine):
-    """ This class provides interface between blender interface and the RenderGirl Core """
-    bl_idname = 'rendergirl'
+    """ This class provides interface between blender UI and the RenderGirl Core """
+    bl_idname = 'RenderGirl'
     bl_label = 'RenderGirl'
     bl_use_preview = True
 
@@ -15,10 +28,15 @@ class RenderGirl(bpy.types.RenderEngine):
 
 
 
-# Register the RenderEngine
-bpy.utils.register_class(RenderGirl)
+def register():
+    # Register the RenderEngine
+    bpy.utils.register_class(RenderGirl)
 
-# Register render properties UI elements
-from bl_ui import properties_render
-properties_render.RENDER_PT_render.COMPAT_ENGINES.add('rendergirl')
-del properties_render
+    # Register render properties UI elements
+    from bl_ui import properties_render
+    properties_render.RENDER_PT_render.COMPAT_ENGINES.add('RenderGirl')
+    del properties_render
+
+
+def unregister():
+    bpy.utils.unregister_class(RenderGirl)
